1b278fb0a3687d1154b90c1651bbe25661af36b4
Danny Fuhriman <danny.fuhriman@Okta.com>
2025-09-24 10:57:42 -0700

    SWR fetcher util

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,17 @@
-import { clsx, type ClassValue } from 'clsx'
-import { twMerge } from 'tailwind-merge'
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs));
 }
+
+export const fetcher = async (url: string) => {
+	if (url.startsWith('config:')) return null;
+
+	const response = await fetch(url);
+	if (!response.ok) {
+		const { code, cause } = await response.json();
+		throw new Error(`${code} | ${cause}`);
+	}
+	return response.json();
+};
